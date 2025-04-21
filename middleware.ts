3b7e04265e3810ba78b6a812a02825240a9de732
Jynkone <ajeenkya29@gmail.com>
2025-04-21 22:48:20 +0530

    Force build

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 // Removed unused import: import { shouldUseAuth } from "@/lib/shouldUseAuth";
 
-// Directly export clerkMiddleware to always enforce authentication
 export default clerkMiddleware();
 
 export const config = {
